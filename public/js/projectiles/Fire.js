952b73599fb71158a6ca0a56ecdb922a2f2988eb
Remi <remi.longin@outlook.fr>
2019-11-29 15:20:42 +0100

    Fix skeleton's bullet size and design

--- public/js/projectiles/Fire.js
+++ public/js/projectiles/Fire.js
@@ -3,18 +3,18 @@ import { Projectile } from '/js/projectiles/Projectile.js';
 export class Fire extends Projectile {
     constructor(context, x, y) {
         super(context, x, y);
-        
+
         this.damageValue = 1;
         this.dotTime = 3000;
     }
 
-    onHit(entity) {        
-        if(entity.canAffect) {            
+    onHit(entity) {
+        if (entity.canAffect) {
             entity.canAffect = false;
             const interval = setInterval(() => {
                 super.onHit(entity);
             }, 100);
-            setTimeout(() => {    
+            setTimeout(() => {
                 entity.canAffect = true;
                 clearInterval(interval);
             }, this.dotTime);
@@ -22,12 +22,12 @@ export class Fire extends Projectile {
         }
     }
 
-    draw() {        
+    draw() {
         this.context.save();
         this.context.translate(this.x, this.y);
-        this.context.fillStyle = "black";
+        this.context.fillStyle = "orange";
         this.context.beginPath();
-        this.context.arc(0, 0, 2, 0, 2 * Math.PI);
+        this.context.arc(0, 0, 5, 0, 2 * Math.PI);
         this.context.fill();
         this.context.restore();
     }
