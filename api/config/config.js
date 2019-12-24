b489e2421c8e0fecec5836276b8ad738b92ad422
Dorian <dorian.chapoulie@gmail.com>
2019-12-24 16:11:38 +0100

    Update config.js

--- api/config/config.js
+++ api/config/config.js
@@ -8,10 +8,4 @@ const config = {
     },
 };
 
-/*mysql: {
-    host: "2a02:4780:bad:c0de::14",
-    user: "id11999135_dorian",
-    password: "buffadungeonheroes06",
-    database: 'id11999135_dungeonheroes',
-},*/
 module.exports = config;
