536ffbe4c222fb6bedd5a7c02e2a24f434f0295b
Chap <dorian.chapoulie@gmail.com>
2019-12-26 15:50:09 +0100

    update base

--- api/lib/dungeonheroes.sql
+++ api/lib/dungeonheroes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  sam. 21 déc. 2019 à 15:23
+-- Généré le :  jeu. 26 déc. 2019 à 14:49
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -25,6 +25,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `inventory`
+--
+
+DROP TABLE IF EXISTS `inventory`;
+CREATE TABLE IF NOT EXISTS `inventory` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `playerid` int(11) NOT NULL,
+  `skinid` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=55 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `inventory`
+--
+
+INSERT INTO `inventory` (`id`, `playerid`, `skinid`) VALUES
+(54, 13, 1),
+(53, 13, 4),
+(52, 13, 2);
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `player`
 --
 
@@ -34,31 +57,55 @@ CREATE TABLE IF NOT EXISTS `player` (
   `pseudo` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `money` int(11) NOT NULL DEFAULT '0',
+  `money` int(11) NOT NULL DEFAULT '5',
   `lootbox` int(11) NOT NULL DEFAULT '0',
-  `skinid` int(11) NOT NULL DEFAULT '0',
+  `skinid` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `pseudo` (`pseudo`)
-) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `player`
 --
 
 INSERT INTO `player` (`id`, `pseudo`, `email`, `password`, `money`, `lootbox`, `skinid`) VALUES
-(1, 'dorian', 'dorian.chapoulie@gmail.com', '1234', 0, 0, 0),
+(1, 'dorian', 'dorian.chapoulie@gmail.com', '1234', 0, 0, 1),
 (2, 'none', 'dorian.chapoulie2@gmail.com', '1234', 680, 0, 0),
 (3, 'none2', 'dorian.chapoulie3@gmail.com', '1234', 0, 0, 0),
 (4, 'none3', 'dorian.chapoulie4@gmail.com', '1234', 0, 0, 0),
 (5, 'none5', 'dorian.chapoulie5@gmail.com', '1234', 0, 0, 0),
 (6, 'a', 'a@a.com', 'a', 0, 0, 0),
 (7, 'fzqfzq', 'ab@a.com', 'a', 0, 0, 0),
-(8, 'aafdf', 'a', 'a', 9738, 297, 1),
+(8, 'aafdf', 'a', 'a', 9621, 402, 1),
 (9, 'aa', 'af@a.com', 'ffzfzfz', 0, 0, 0),
 (10, 'chap', 'dodo@dodo.com', 'bouffon', 0, 0, 0),
 (11, 'test', 'test@test.com', 'test', 0, 0, 0),
-(12, 'xena', 'ch@gmail.com', '1234', 0, 0, 0);
+(12, 'xena', 'ch@gmail.com', '1234', 0, 0, 0),
+(13, 'ab', 'toto', 'a', 0, 2, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `skin`
+--
+
+DROP TABLE IF EXISTS `skin`;
+CREATE TABLE IF NOT EXISTS `skin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `path` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `skin`
+--
+
+INSERT INTO `skin` (`id`, `path`) VALUES
+(1, '/images/skin0.png'),
+(2, '/images/skin1.png'),
+(3, '/images/skin2.png'),
+(4, '/images/skin3.png');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
